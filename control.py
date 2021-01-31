33ebb6f62fb6b536c8afa504fa4d70ef90cfae84
hxytoday <976396706@qq.com>
2021-01-31 22:44:48 +0800

    fix

--- control.py
+++ control.py
@@ -2,10 +2,22 @@ from machine import Pin, PWM
 from neopixel import NeoPixel
 import random
 import time
+from web import param_data
+from config import read_conf
 
 pin = Pin(22, Pin.OUT)
 np = NeoPixel(pin, 30)
 s1 = PWM(Pin(18), freq=50, duty=0)
+autoflag = read_conf(autoflay)
+'''
+自动参数
+水泵延时valuedelay
+抽水时间valuetime
+抽水间隔valueinterval
+喷雾延时fogdelay
+加热温度
+
+'''
 
 
 def color_buf(mode, delay=0):
@@ -33,3 +45,8 @@ def light(mode):  # on off
 
 def servo(angle):
     s1.duty(int(((angle + 90) * 2 / 180 + 0.5) / 20 * 1023))
+
+
+def auto():
+    while autoflag == 1:
+        pass
