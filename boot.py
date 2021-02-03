5ffbff4231f33d1326dc0de97913ac9564938452
hxytoday <976396706@qq.com>
2021-02-03 22:58:07 +0800

    配置读取和写操作

--- boot.py
+++ boot.py
@@ -5,9 +5,9 @@ from config import read_param
 
 read_param()
 
-KEY = Pin(0, Pin.IN, Pin.PULL_UP)
-
-if KEY.value() == 1:
-    time.sleep_ms(200)
-    if KEY.value() == 1:
-        do_ap()
+# KEY = Pin(0, Pin.IN, Pin.PULL_UP)
+#
+# if KEY.value() == 1:
+#     time.sleep_ms(200)
+#     if KEY.value() == 1:
+#         do_ap()
